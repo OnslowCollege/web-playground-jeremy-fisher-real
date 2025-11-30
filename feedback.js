fc79be59b81245784c11f3cc867923795383582a
jeremy-fisher-real <leroy.jacobs@student.onslow.school.nz>
2025-12-01 08:53:41 +1300

    Add files via upload

--- feedback.js
+++ feedback.js
@@ -1,21 +1,13 @@
-// const feedback_input = document.getElementById(`feedback_input`)
-// const feedback_button = document.getElementById(`feedback_button`)
-
-// feedback_button.addEventListener("click", function(event) {
-// console.log(feedback_input.value)
-// feedback_input.value = ``
-// })
-
-// console.log(feedback_input, feedback_button)
-
 function triggerAnimation() {
     const feedback_input = document.getElementById(`feedback_input`)
-    feedback_input.classList.add('feedback_animate');
-
-    // Optional: Remove the class to allow re-triggering
+    feedback_input.style.animationName = `trash_animation`
+    feedback_input.style.animationDuration = `6s`
+    feedback_input.style.animationTimingFunction = `linear`
+    feedback_input.disabled = true
     feedback_input.addEventListener('animationend', function handler() {
+        feedback_input.style.animation = ``
         feedback_input.value = ``
-        feedback_input.classList.remove('feedback_animate');
-        feedback_input.removeEventListener('animationend', handler); // Remove listener to prevent multiple calls
-    });
+        feedback_input.disabled = false
+        feedback_input.removeEventListener('animationend', handler)
+    })
 }
